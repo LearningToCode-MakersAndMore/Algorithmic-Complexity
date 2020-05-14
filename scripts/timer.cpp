a6409b57764b86a97146a820db45dce1348b1bab
Peter <pc.dean@icloud.com>
2020-05-14 21:49:51 +0100

    Modify timer function to default to empty vector and template T1 to default to vecotr<int>

--- scripts/timer.cpp
+++ scripts/timer.cpp
@@ -2,11 +2,11 @@
 #include<vector>
 using namespace std;
 
-template<typename T0, typename T1=int>
-void timer(T0 func, T1 arg=0)
+template<typename T0, typename T1=vector<int> >
+void timer(T0 func, T1 arg = *(new vector<int>))
 {
 
-  *func << arg;
+  *func << arg[0];
 
 }
 

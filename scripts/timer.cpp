e539e6e1a1e8262839e03d48d8ba07f69ee240c3
Peter <pc.dean@icloud.com>
2020-05-17 14:18:56 +0100

    Remove unesisary complexity of default new vector to arg paramater as unesisary

--- scripts/timer.cpp
+++ scripts/timer.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 template<typename T0, typename T1=vector<int> >
-void timer(T0 func, T1 arg = *(new vector<int>))
-{
+void timer(T0 func, T1 arg) {
 
   *func << &arg;
 
 }
 
 int main() {
-  timer(&cout);
+  timer(&cout, {});
 }

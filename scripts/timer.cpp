5deff81b1ea650cb5906214e6159bb2bdc856b6e
Peter <pc.dean@icloud.com>
2020-05-19 19:43:28 +0100

    succesfully test execution of pointer to method function

--- scripts/timer.cpp
+++ scripts/timer.cpp
@@ -15,9 +15,9 @@ int timer(T0 func) {
 }
 
 template<typename T0, typename T1>
-void testFunc(T0 object, T1 (string::*func)(void) ) {
+void testFunc(T0 object, T1 method ) {
   auto start = high_resolution_clock::now();
-  object.func;
+  cout << method;
   auto stop = high_resolution_clock::now();
   int duration =  duration_cast<microseconds>(stop-start).count();
   cout << duration;
@@ -34,10 +34,16 @@ int main() {
   // string::size_type* (string::*method())() = string::length;
   // string (string::*test)() = &(string::length);
   // string* cat;
-  // string bob = "hello";
   // testFunc(bob, cat->length());
-  typedef void (string::*strMemPtr)();
-  strMemPtr strMem = &string::clear;
+  // typedef void (string::*strMemPtr)();
+  // strMemPtr strMem = &string::clear;
   // b= &string::length;
   // cout << b;
+  string bob = "hello";
+  typedef void (string::*MethodPtr)();
+  MethodPtr method = &string::clear;
+  (bob.*method)();
+  cout << bob;
+  // cout << bob;
+  // testFunc(bob, method);
 }

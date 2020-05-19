858053ba5cfa00d945db8d281d8d1ebad922ab76
Peter <pc.dean@icloud.com>
2020-05-19 18:48:53 +0100

    implement typedef to define member function pointer

--- scripts/timer.cpp
+++ scripts/timer.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<chrono>
+#include<string>
 using namespace std;
 using namespace std::chrono;
 
@@ -13,12 +14,14 @@ int timer(T0 func) {
   return duration;
 }
 
-// template<typename T0, typename T1=void>
-// void testFunc(T0 func, T1 funcParam ) {
-//   T0 (*)
-//   auto (T0::*)(T1 funcParam)
-//
-// }
+template<typename T0, typename T1>
+void testFunc(T0 object, T1 (string::*func)(void) ) {
+  auto start = high_resolution_clock::now();
+  object.func;
+  auto stop = high_resolution_clock::now();
+  int duration =  duration_cast<microseconds>(stop-start).count();
+  cout << duration;
+}
 
 int foo() {
   cout << "hello";
@@ -27,5 +30,14 @@ int foo() {
 
 int main() {
   // int (*bar)() = cout;
-  cout << timer(foo);
+  // cout << timer(foo);
+  // string::size_type* (string::*method())() = string::length;
+  // string (string::*test)() = &(string::length);
+  // string* cat;
+  // string bob = "hello";
+  // testFunc(bob, cat->length());
+  typedef void (string::*strMemPtr)();
+  strMemPtr strMem = &string::clear;
+  // b= &string::length;
+  // cout << b;
 }

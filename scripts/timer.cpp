af834a1ffbadc1b4b046e03dcf1c9e2799a58cca
Peter <pc.dean@icloud.com>
2020-05-20 17:01:53 +0100

    basic implementation with templates and using

--- scripts/timer.cpp
+++ scripts/timer.cpp
@@ -5,19 +5,24 @@
 using namespace std;
 using namespace std::chrono;
 
-template<typename T0>
-int timer(T0 func) {
-  auto start = high_resolution_clock::now();
-  func();
-  auto stop = high_resolution_clock::now();
-  int duration =  duration_cast<microseconds>(stop-start).count();
-  return duration;
-}
+// template<typename T0>
+// int timer(T0 func) {
+//   auto start = high_resolution_clock::now();
+//   func();
+//   auto stop = high_resolution_clock::now();
+//   int duration =  duration_cast<microseconds>(stop-start).count();
+//   return duration;
+// }
 
-template<typename T0, typename T1>
-void testFunc(T0 object, T1 method ) {
+template<typename T0>
+using MethodPtr = void (T0::*)();
+
+template<typename T1>
+void testFunc(T1 object, MethodPtr<T1> method ) {
   auto start = high_resolution_clock::now();
-  cout << method;
+  cout << object;
+  (object.*method)();
+  cout << object;
   auto stop = high_resolution_clock::now();
   int duration =  duration_cast<microseconds>(stop-start).count();
   cout << duration;
@@ -29,6 +34,16 @@ int foo() {
 }
 
 int main() {
+  string bob = "hello";
+  testFunc(bob, &string::clear);
+
+  // string bob = "hello";
+  // // typedef void (string::*MethodPtr)();
+  // using MethodPtr = void (string::*)();
+  // MethodPtr method = &string::clear;
+  // (bob.*method)();
+  // cout << bob;
+
   // int (*bar)() = cout;
   // cout << timer(foo);
   // string::size_type* (string::*method())() = string::length;
@@ -39,12 +54,12 @@ int main() {
   // strMemPtr strMem = &string::clear;
   // b= &string::length;
   // cout << b;
-  string bob = "hello";
-  // typedef void (string::*MethodPtr)();
-  using MethodPtr = void (string::*)();
-  MethodPtr method = &string::clear;
-  (bob.*method)();
-  cout << bob;
+  // string bob = "hello";
+  // // typedef void (string::*MethodPtr)();
+  // using MethodPtr = void (string::*)();
+  // MethodPtr method = &string::clear;
+  // (bob.*method)();
+  // cout << bob;
   // cout << method;
   // (bob->*method)();
   // void (string::*method)() = &string::clear;
